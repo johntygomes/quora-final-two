cc55c2cc1787943b68a7c14f4f3d40b65bd778b8
roadcastIntern <intern@roadcast.in>
2021-06-27 16:33:58 +0530

    fixed redirect issue

--- main/views.py
+++ main/views.py
@@ -58,6 +58,7 @@ def newQuestionPage(request):
                 question = form.save(commit=False)
                 question.author = request.user
                 question.save()
+                return redirect('index')
         except Exception as e:
             print(e)
             raise
