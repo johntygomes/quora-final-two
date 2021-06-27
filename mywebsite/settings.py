d322d0bbd65ca9d86f02380067cb692c87a8df9a
roadcastIntern <intern@roadcast.in>
2021-06-27 15:49:18 +0530

    added static settings for hosting

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +134,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static')
+     os.path.join(BASE_DIR, 'main/static')
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
